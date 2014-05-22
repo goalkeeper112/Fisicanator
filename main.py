3a0d91a5ca95e713fcd38c2cf1556c3e2b58aba5
goalkeeper112 <lfbu.112@gmail.com>
2014-05-21 21:35:26 -0430

    Actualización en donde se agrega la resolución de cantidad de calor

--- main.py
+++ main.py
@@ -2,29 +2,65 @@
 
 # Saludo y entrada del script en python desarrollado por Aponce y Goalkeeper112
 
-print '#####################################################'
-print '#######           Fisicanator               #########'
-print '#######     Un srcipt para determinar       #########' 
-print '####### la cantidad de calor de un objeto   #########'
-print '#######                                     #########'
-print '#######     Desarrollado Por Aponce y       #########'
-print '#######          Goalkeeper112              #########'
-print '#####################################################'
+print '#################################################################'
+print '#######                Fisicanator                      #########'
+print '#######           Un srcipt para Resolver               #########' 
+print '#######    La mayoria de los problemas planteados       #########'
+print '#######        en la fisica 1 de Bachillerato           #########'
+print '#######                                                 #########'
+print '#######         Desarrollado Por Aponce y               #########'
+print '#######              Goalkeeper112                      #########'
+print '#################################################################'
 
-print 'Digite los valores necesarios para el calculo'
+tema = (raw_input("Tema a resolver: "))
 
-# En esta parte se digitan los datos numericos de la ecuacion
+if(tema == "calor"):
+	# En esta parte se digitan los datos numericos de la ecuacion
+	masa = input("masa: ")
+	t1   = input("temperatura Inicial: ")
+	t2   = input("temperatura final: ")
+	# En esta parte se pide la digitacion del material con el cual se esta trabajando
+	ce   = (raw_input("material: "))
 
-masa = input("masa: ")
-t1   = input("temperatura Inicial: ")
-t2   = input("temperatura final: ")
-# En esta parte se pide la digitacion del material con el cual se esta trabajando
-
-ce   = (raw_input("material: "))
-
-if(ce == 'aluminio'):
-	ce = 0.22
-	q  = masa * ce * (t2 - t1)
-	print q
-else:
-	print "No se puede resolver la ecuacion"
+	if(ce == 'aluminio'):
+		medida = (raw_input("Con cual unidad va a trabajar: "))
+		if(medida == "j"):
+			ce = 898
+			q  = masa * ce * (t2 - t1)
+			print q
+		elif(medida == "cal"):
+			ce = 0.22
+			q  = masa * ce * (t2 - t1)
+			print q
+		else:
+			print "No se pudo resolver la ecuación debido a que ocurrio un error"
+	elif(ce == 'cobre'):
+		medida = (raw_input("Con cual unidad va a trabajar:"))
+		if(medida == "j"):
+			ce = 385
+			q  = masa * ce * (t2 - t1)
+			print q
+		elif(medida == "cal"):
+			ce = 0.09
+			q  = masa * ce * (t2 - t1)
+			print q
+		else:
+			print "No se pudo resolver la ecuación debido a que ocurrio un error"
+	elif(ce == "plomo"):
+		medida = (raw_input("Con cual unidad va a trabajar:"))
+		if(medida == "j"):
+			ce = 125
+			q  = masa * ce * (t2 - t1)
+			print q
+		elif(medida == "cal"):
+			ce = 0.03
+			q  = masa * ce * (t2 - t1)
+			print q
+		else:
+			print "No se pudo resolver la ecuación debido a que ocurrio un error"
+	elif(ce == "aceite"):
+		ce = 0.47
+		q  = masa * ce * (t2 - t1)
+		print q 
+	else:
+		print "No se puede resolver la ecuacion"
